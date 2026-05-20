92360cda1742e4f4208b8d92ff5ac57d439283ee
Michal Dengusiak <tassimulation@gmail.com>
2026-05-20 20:08:52 +0200

    fix: replace literal u{2013} escape with actual en-dash in SPDX header
    PowerShell 5.1 doesn't support backtick-u escape sequences; the
    apply-stage2.ps1 script leaked them as literal text in 'Copyright (c) 2020u{2013}2026'.

--- Grasshopper/SAM.Analytical.Grasshopper.gbXML/Kernel/AssemblyInfo.cs
+++ Grasshopper/SAM.Analytical.Grasshopper.gbXML/Kernel/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿// SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright (c) 2020u{2013}2026 Michal Dengusiak & Jakub Ziolkowski and contributors
+// Copyright (c) 2020–2026 Michal Dengusiak & Jakub Ziolkowski and contributors
 
 using Grasshopper.Kernel;
 using System;
